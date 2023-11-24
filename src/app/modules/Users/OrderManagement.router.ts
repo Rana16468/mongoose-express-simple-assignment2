eb45c4784e2d89345457564315419a97e199cfbd
Rana16468 <amsohelrana.me@gmail.com>
2023-11-24 22:11:32 +0600

    endpoint set and final commit

--- src/app/modules/Users/OrderManagement.router.ts
+++ src/app/modules/Users/OrderManagement.router.ts
@@ -4,14 +4,14 @@ import { UsersControllers } from './OrderManagement.controller';
 
 const router=express.Router();
 
-router.post('/users',UsersControllers.createUserController);
-router.get('/users',UsersControllers.All_Users_Controller);
-router.get('/users/:userId',UsersControllers.specificUser_by_ID);
-router.put('/users/:userId',UsersControllers.Update_User_Information);
-router.delete('/users/:userId',UsersControllers.deleteUser);
+router.post('/',UsersControllers.createUserController);
+router.get('/',UsersControllers.All_Users_Controller);
+router.get('/:userId',UsersControllers.specificUser_by_ID);
+router.put('/:userId',UsersControllers.Update_User_Information);
+router.delete('/:userId',UsersControllers.deleteUser);
 //oreder routers 
-router.put('/users/:userId/orders',UsersControllers.product_Order);
-router.get('/users/:userId/orders',UsersControllers.specific_User_Order);
-router.get('/users/:userId/orders/total-price',UsersControllers.calculateTotalPrice_SpecificOrder);
+router.put('/:userId/orders',UsersControllers.product_Order);
+router.get('/:userId/orders',UsersControllers.specific_User_Order);
+router.get('/:userId/orders/total-price',UsersControllers.calculateTotalPrice_SpecificOrder);
 
 export const UsersRouter=router;
